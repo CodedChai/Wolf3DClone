27f1a8094d4dd23399a2cd1584e9b0c595042db6
Skrillcon <guardianx515@gmail.com>
2017-11-16 14:48:31 -0600

    Loading textures from 1 file

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -14,10 +14,13 @@ public class Game {
     private static final float SPOT_LENGTH = 1f;
     private static final float SPOT_HEIGHT = 1f;
 
+    private static final int NUM_TEX_EXPONENT = 4;
+    private static final int NUM_TEXTURES = (int)Math.pow(2, NUM_TEX_EXPONENT);
+
     public Game(){
         level = new Bitmap("TestLevel.png").flipY();
         shader = BasicShader.getInstance();
-        material = new Material(new Texture("Marble.jpg"));
+        material = new Material(new Texture("WolfCollection.png"));
 
         ArrayList<Vertex> vertices = new ArrayList<Vertex>();
         ArrayList<Integer> indices = new ArrayList<Integer>();
@@ -28,10 +31,14 @@ public class Game {
                 if((level.getPixel(i,j) & 0xFFFFFF) == 0){
                     continue;
                 }
-                float XHigher = 1;
-                float XLower = 0;
-                float YHigher = 1;
-                float YLower = 0;
+                int texX = ((level.getPixel(i,j) & 0x00FF00) >> 8) / NUM_TEXTURES; // Green component
+                int texY = texX % NUM_TEX_EXPONENT;
+                texX /= NUM_TEX_EXPONENT;
+
+                float XHigher = 1f - (float)texX/(float)NUM_TEX_EXPONENT;
+                float XLower = XHigher - 1f/(float)NUM_TEX_EXPONENT;
+                float YLower= 1f - (float)texY/(float)NUM_TEX_EXPONENT;
+                float YHigher = YLower - 1f/(float)NUM_TEX_EXPONENT;
 
                 // Generate Floor
                 indices.add(vertices.size() + 2);
@@ -58,6 +65,15 @@ public class Game {
                 vertices.add(new Vertex(new Vector3f((i + 1) * SPOT_WIDTH,SPOT_HEIGHT,j * SPOT_LENGTH), new Vector2f(XHigher,YLower)));
                 vertices.add(new Vertex(new Vector3f((i + 1) * SPOT_WIDTH,SPOT_HEIGHT,(j + 1) * SPOT_LENGTH), new Vector2f(XHigher,YHigher)));
                 vertices.add(new Vertex(new Vector3f(i * SPOT_WIDTH,SPOT_HEIGHT,(j + 1) * SPOT_LENGTH), new Vector2f(XLower,YHigher)));
+
+                texX = ((level.getPixel(i,j) & 0xFF0000) >> 16) / NUM_TEXTURES; // Red component
+                texY = texX % NUM_TEX_EXPONENT;
+                texX /= NUM_TEX_EXPONENT;
+
+                XHigher = 1f - (float)texX/(float)NUM_TEX_EXPONENT;
+                XLower = XHigher - 1f/(float)NUM_TEX_EXPONENT;
+                YLower= 1f - (float)texY/(float)NUM_TEX_EXPONENT;
+                YHigher = YLower - 1f/(float)NUM_TEX_EXPONENT;
 
                 // Generate Walls
                 if((level.getPixel(i,j - 1) & 0xFFFFFF) == 0){
--- src/com/base/engine/Texture.java
+++ src/com/base/engine/Texture.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 
 public class Texture {
     private int id;
@@ -36,6 +37,7 @@ public class Texture {
         BufferedImage image;
 
         try {
+
             image = ImageIO.read(new File("./res/textures/" + fileName));
 
             int[] pixels = new int[image.getWidth() * image.getHeight()];
@@ -69,7 +71,7 @@ public class Texture {
             //Setup texture scaling filtering
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+            glGenerateMipmap(GL_TEXTURE_2D);
             //Send texel data to OpenGL
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 
